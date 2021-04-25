c4f7728ac17d57fd11501f1711ead06433bf6b12
Melih Ekici <melih.ekici4@gmail.com>
2021-04-25 14:28:53 +0300

    Building portfolio

---
+++ project2.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const classificationImageButton = document.querySelector('input[type="file"]');
+console.log(classificationImageButton);
+
+classificationImageButton.addEventListener('change', function(){
+    if(this.files && this.files[0]) {
+        const fileName = this.files[0]['name'].toLowerCase();
+        if(fileName.includes('.jpg') || fileName.includes('.png') || fileName.includes('.jpeg')){
+            var img = document.querySelector('#myImg');
+            img.onload = () => {
+                URL.revokeObjectURL(img.src);  // no longer needed, free memory
+            }
+    
+            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+
+            classify();
+        }
+    }
+})
+
+function classify() {
+    const formData = new FormData();
+    formData.append("image", classificationImageButton.files[0]);
+
+    const classificationRequest = new XMLHttpRequest();
+    classificationRequest.open("POST", "http://54.237.55.50:5000/classify")
+    //classificationRequest.open("GET", "http://google.com.tr");
+    console.log(classificationRequest.send(formData)); 
+}
